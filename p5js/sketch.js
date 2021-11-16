002d6c12d6d87cd6aa6303db8369de72a3cad88f
Spencer Miller <spence.miller@gmail.com>
2021-11-16 12:32:36 -0800

    [p5] remove Math.floor call on colors, also format

--- p5js/sketch.js
+++ p5js/sketch.js
@@ -12,19 +12,19 @@ function setup() {
   createCanvas(windowWidth, windowHeight);
 
   // set up the fixed attributes of the generator
-  red = Math.floor(255 * fxrand());
-  green = Math.floor(255 * fxrand());
-  blue = Math.floor(255 * fxrand());
+  red = 255 * fxrand();
+  green = 255 * fxrand();
+  blue = 255 * fxrand();
   numDots = Math.floor(fxrand() * 10) + 1;
 
   // assign them to the $fxhashFeatures object
   window.$fxhashFeatures = {
     // with descriptive names
-    "Red-ness": red > 240 ? "Crimson" : "Classic Red",
-    "Green-ness": green > 240 ? "Verdant" : "Classic Green",
-    "Blue-ness": blue > 240 ? "Royal" : "Classic Blue",
+    'Red-ness': red > 240 ? 'Crimson' : 'Classic Red',
+    'Green-ness': green > 240 ? 'Verdant' : 'Classic Green',
+    'Blue-ness': blue > 240 ? 'Royal' : 'Classic Blue',
     // or just the value
-    "Number of Dots": numDots,
+    'Number of Dots': numDots,
   };
 }
 
@@ -36,12 +36,11 @@ function draw() {
   var h2 = windowHeight / 2;
   var w2 = windowWidth / 2;
   var timer = frameCount * 0.05;
-  
+
   for (var i = 0; i < numDots; i++) {
     var rotation = i / numDots;
     ellipse(
-      w2 + sin(timer + TAU * rotation) * h4, 
-      h2 + cos(timer + TAU * rotation) * h4, 
-      h8, h8);
+        w2 + sin(timer + TAU * rotation) * h4,
+        h2 + cos(timer + TAU * rotation) * h4, h8, h8);
   }
 }
